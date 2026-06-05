530431b867393ed8ffc85ca1f16ed1921644104f
Zsolt Andre <zsolt_andre@epam.com>
2026-06-05 09:44:34 +0200

    lesson5: practice2

--- lesson5/functionalProgramming.ts
+++ lesson5/functionalProgramming.ts
@@ -8,6 +8,7 @@ const numberArray: number[] = [1, 2, 3];
 const mapArray = (arr: number[], fn: (x: number) => number): number[] => arr.map(fn);
 
 const multiplyByTwo = (x: number): number => x * 2;
+const addOne = (x: number): number => x + 1;
 
 const result = mapArray(numberArray, multiplyByTwo);
 console.log("Result of mapping:", result); // [2, 4, 6]
@@ -16,3 +17,7 @@ const multiply = (a : number) => (b: number): number => a * b;
 
 const curryResult = multiply(5)(3);
 console.log("Result of currying:", curryResult); // 15
+
+const composedFunction = (x: number): number => addOne(multiplyByTwo(x));
+const composedResult = composedFunction(4);
+console.log("Result of function composition:", composedResult); // 9
