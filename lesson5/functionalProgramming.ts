66b302b96cf910b815179daafac7ef2caab09a76
Zsolt Andre <zsolt_andre@epam.com>
2026-06-05 09:43:17 +0200

    lesson5: practice

---
+++ lesson5/functionalProgramming.ts
@@ -0,0 +1,18 @@
+const add = (a: number, b: number): number => a + b;
+
+const readOnlyArray: ReadonlyArray<number> = [1, 2, 3];
+// readOnlyArray.push(4); // Error: Property 'push' does not exist on type 'readonly number[]'.
+
+const numberArray: number[] = [1, 2, 3];
+
+const mapArray = (arr: number[], fn: (x: number) => number): number[] => arr.map(fn);
+
+const multiplyByTwo = (x: number): number => x * 2;
+
+const result = mapArray(numberArray, multiplyByTwo);
+console.log("Result of mapping:", result); // [2, 4, 6]
+
+const multiply = (a : number) => (b: number): number => a * b;
+
+const curryResult = multiply(5)(3);
+console.log("Result of currying:", curryResult); // 15
