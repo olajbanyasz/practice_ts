378c4c554d68d03a1ef11ccce9bac8e8ae8428cb
Zsolt Andre <zsolt_andre@epam.com>
2026-06-08 10:34:22 +0200

    Remove node_modules from git tracking

---
+++ lesson6/apiService.d.ts
@@ -0,0 +1,6 @@
+export declare class ApiService {
+    private baseUrl;
+    constructor(baseUrl: string);
+    fetchData(endpoint: string): Promise<any>;
+}
+//# sourceMappingURL=apiService.d.ts.map
