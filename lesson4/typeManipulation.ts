47904b329a1117fc756bb79ba2bab5fc202f2728
Zsolt Andre <zsolt_andre@epam.com>
2026-06-12 12:46:16 +0200

    lesson8: lesson4 folder fix

--- lesson4/typeManipulation.ts
+++ lesson4/typeManipulation.ts
@@ -4,7 +4,7 @@ export interface Person {
   id: number;
   name: string;
   email: string;
-};
+}
 
 export type Optional<T> = {
   [P in keyof T]?: T[P];
@@ -12,8 +12,8 @@ export type Optional<T> = {
 
 export const person: Person = {
   id: 1,
-  name: "John Doe",
-  email: "john.doe@example.com"
+  name: 'John Doe',
+  email: 'john.doe@example.com',
 };
 
 /* Example of using Person type directly shows that email is required, while OptionalPerson allows it to be optional
