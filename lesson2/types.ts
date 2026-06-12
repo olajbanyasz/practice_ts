3720c6ee10f5fe405ba69ce3cbd34cd0ed781b3f
Zsolt Andre <zsolt_andre@epam.com>
2026-06-12 11:44:34 +0200

    lesson8: lesson2 folder fix

--- lesson2/types.ts
+++ lesson2/types.ts
@@ -1,21 +1,25 @@
 interface Admin {
-    name: string;
-    privileges: string[];
+  name: string;
+  privileges: string[];
 }
 
 interface Employee {
-    name: string;
-    startDate: Date;
+  name: string;
+  startDate: Date;
 }
 
 type ElevatedEmployee = Admin & Employee;
 
-function createElevatedEmployee(name: string, privileges: string[], startDate: Date): ElevatedEmployee {
-    return {
-        name,
-        privileges,
-        startDate
-    };
+function createElevatedEmployee(
+  name: string,
+  privileges: string[],
+  startDate: Date
+): ElevatedEmployee {
+  return {
+    name,
+    privileges,
+    startDate,
+  };
 }
 
 const elevatedEmployee = createElevatedEmployee('Charlie', ['admin'], new Date());
@@ -24,36 +28,36 @@ console.log(elevatedEmployee);
 type User = Admin | Employee;
 
 function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
-    return Object.assign({}, objA, objB);
+  return Object.assign({}, objA, objB);
 }
 
 const admin: Admin = {
-    name: "Alice",
-    privileges: ["admin"]
+  name: 'Alice',
+  privileges: ['admin'],
 };
 
 const employee: Employee = {
-    name: "Bob",
-    startDate: new Date()
+  name: 'Bob',
+  startDate: new Date(),
 };
 
 const mergedUser = merge(admin, employee);
 console.log(mergedUser);
 
 function isAdmin(user: User): user is Admin {
-    return 'privileges' in user;
+  return 'privileges' in user;
 }
 
-console.log(isAdmin(mergedUser) ? "User is an admin" : "User is not an admin");
-console.log(isAdmin(employee) ? "Employee is an admin" : "Employee is not an admin");
-console.log(isAdmin(admin) ? "Admin is an admin" : "Admin is not an admin");
+console.log(isAdmin(mergedUser) ? 'User is an admin' : 'User is not an admin');
+console.log(isAdmin(employee) ? 'Employee is an admin' : 'Employee is not an admin');
+console.log(isAdmin(admin) ? 'Admin is an admin' : 'Admin is not an admin');
 
 function printEmployeeInformation(user: User) {
-    if (isAdmin(user)) {
-        console.log(`Admin: ${user.name}, Privileges: ${user.privileges.join(", ")}`);
-    } else {
-        console.log(`Employee: ${user.name}, Start Date: ${user.startDate}`);
-    }
+  if (isAdmin(user)) {
+    console.log(`Admin: ${user.name}, Privileges: ${user.privileges.join(', ')}`);
+  } else {
+    console.log(`Employee: ${user.name}, Start Date: ${user.startDate}`);
+  }
 }
 
 printEmployeeInformation(mergedUser);
