2be46808b4a46f7e9bbbf89e8996fffcafbd2d6e
Zsolt Andre <zsolt_andre@epam.com>
2026-06-12 13:40:29 +0200

    lesson8: lesson5 folder fix

--- lesson5/functionalProgramming.ts
+++ lesson5/functionalProgramming.ts
@@ -15,13 +15,16 @@ const multiplyByTwo = (x: number): number => x * 2;
 const addOne = (x: number): number => x + 1;
 
 const result = mapArray(numberArray, multiplyByTwo);
-console.log("Result of mapping:", result); // [2, 4, 6]
+console.log('Result of mapping:', result); // [2, 4, 6]
 
-const multiply = (a : number) => (b: number): number => a * b;
+const multiply =
+  (a: number) =>
+  (b: number): number =>
+    a * b;
 
 const curryResult = multiply(5)(3);
-console.log("Result of currying:", curryResult); // 15
+console.log('Result of currying:', curryResult); // 15
 
 const composedFunction = (x: number): number => addOne(multiplyByTwo(x));
 const composedResult = composedFunction(4);
-console.log("Result of function composition:", composedResult); // 9
+console.log('Result of function composition:', composedResult); // 9
--- lesson5/objectOrientedProgramming.ts
+++ lesson5/objectOrientedProgramming.ts
@@ -1,45 +1,44 @@
 interface PersonInterface {
-    getFullName(): string;
+  getFullName(): string;
 }
 
 class Person implements PersonInterface {
-    private firstName: string;
-    private lastName: string;
+  private firstName: string;
+  private lastName: string;
 
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-    public getFullName(): string {
-        return `${this.firstName} ${this.lastName}`;
-    }
+  public getFullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
 class Student extends Person implements PersonInterface {
-    private grade: number;
+  private grade: number;
 
-    constructor(firstName: string, lastName: string, grade: number) {
-        super(firstName, lastName);
-        this.grade = grade;
-    }
+  constructor(firstName: string, lastName: string, grade: number) {
+    super(firstName, lastName);
+    this.grade = grade;
+  }
 
-    public getStudentInfo(): string {
-        return `${this.getFullName()} - Grade: ${this.grade}`;
-    }
+  public getStudentInfo(): string {
+    return `${this.getFullName()} - Grade: ${this.grade}`;
+  }
 }
 
 const people: PersonInterface[] = [
-    new Person('Anna', 'Nagy'),
-    new Student('Bence', 'Kiss', 5),
-    new Person('Csilla', 'Tóth')
+  new Person('Anna', 'Nagy'),
+  new Student('Bence', 'Kiss', 5),
+  new Person('Csilla', 'Tóth'),
 ];
 
 people.forEach((person) => {
-    if (person instanceof Student) {
-        console.log((person as Student).getStudentInfo());
-    } else {
-        console.log(person.getFullName());
-    }
+  if (person instanceof Student) {
+    console.log((person as Student).getStudentInfo());
+  } else {
+    console.log(person.getFullName());
+  }
 });
-
